8c9c5d084f769196c29345652ea7a6e8fdc8fb70
Davi Barbosa Vieira Bispo <davib.g767@gmail.com>
2022-11-21 23:55:31 -0300

    fix position of chess pieces in chessBoard

--- chess/app/src/main/java/com/example/chess/ChessBoardActivity.java
+++ chess/app/src/main/java/com/example/chess/ChessBoardActivity.java
@@ -66,7 +66,7 @@ public class ChessBoardActivity extends AppCompatActivity {
                 });
 
                 chessBoardGrid.addView(squared, coordinates); // adicionando uma nova casa no tabuleiro
-                chessBoard.addSquared(i, cont, squared);
+                chessBoard.addSquared(cont, i, squared);
                 cont--;
             }
         }// fim do processo de adicionar as casas do tabuleiro
@@ -77,12 +77,18 @@ public class ChessBoardActivity extends AppCompatActivity {
             ImageView pawnDarkImg = new ImageView(this);
             pawnDarkImg.setImageResource(R.drawable.pawndark);
             chessBoard.addPiece(new Pawn(EnumColor.DARK, 6, i, pawnDarkImg));
+            chessBoard.getPiece(6, i).getImage().setLayoutParams(chessBoard.getSquared(6, i).getLayoutParams());
+            chessBoardGrid.addView(chessBoard.getPiece(6, i).getImage());
+
             ImageView pawnWhiteImg = new ImageView(this);
-            chessBoard.addPiece(new Pawn(EnumColor.DARK, 1, i, pawnWhiteImg));
+            pawnWhiteImg.setImageResource(R.drawable.pawnwhite);
+            chessBoard.addPiece(new Pawn(EnumColor.WHITE, 1, i, pawnWhiteImg));
+            chessBoard.getPiece(1, i).getImage().setLayoutParams(chessBoard.getSquared(1, i).getLayoutParams());
+            chessBoardGrid.addView(chessBoard.getPiece(1, i).getImage());
 
         }
 
-        ImageView bishopDark1Img = new ImageView(this, null, 0);
+        ImageView bishopDark1Img = new ImageView(this);
         ImageView bishopDark2Img = new ImageView(this);
         ImageView rookDark1Img = new ImageView(this);
         ImageView rookDark2Img = new ImageView(this);
@@ -90,6 +96,14 @@ public class ChessBoardActivity extends AppCompatActivity {
         ImageView knightDark2Img =  new ImageView(this);
         ImageView queenDarkImg =  new ImageView(this);
         ImageView kingDarkImg =  new ImageView(this);
+        ImageView bishopWhite1Img = new ImageView(this);
+        ImageView bishopWhite2Img = new ImageView(this);
+        ImageView rookWhite1Img = new ImageView(this);
+        ImageView rookWhite2Img = new ImageView(this);
+        ImageView knightWhite1Img =  new ImageView(this);
+        ImageView knightWhite2Img =  new ImageView(this);
+        ImageView queenWhiteImg =  new ImageView(this);
+        ImageView kingWhiteImg =  new ImageView(this);
         bishopDark1Img.setImageResource(R.drawable.bishopdark);
         bishopDark2Img.setImageResource(R.drawable.bishopdark);
         rookDark1Img.setImageResource(R.drawable.rookdark);
@@ -98,22 +112,62 @@ public class ChessBoardActivity extends AppCompatActivity {
         knightDark2Img.setImageResource(R.drawable.knightdark);
         queenDarkImg.setImageResource(R.drawable.queendark);
         kingDarkImg.setImageResource(R.drawable.kingdark);
+        bishopWhite1Img.setImageResource(R.drawable.bishopwhite);
+        bishopWhite2Img.setImageResource(R.drawable.bishopwhite);
+        rookWhite1Img.setImageResource(R.drawable.rookwhite);
+        rookWhite2Img.setImageResource(R.drawable.rookwhite);
+        knightWhite1Img.setImageResource(R.drawable.knightwhite);
+        knightWhite2Img.setImageResource(R.drawable.knightwhite);
+        queenWhiteImg.setImageResource(R.drawable.queenwhite);
+        kingWhiteImg.setImageResource(R.drawable.kingwhite);
         chessBoard.addPiece(new Bishop(EnumColor.DARK, 7, 2, bishopDark1Img));
         chessBoard.addPiece(new Bishop(EnumColor.DARK, 7, 5, bishopDark2Img));
         chessBoard.addPiece(new Knight(EnumColor.DARK, 7, 1, knightDark1Img));
         chessBoard.addPiece(new Knight(EnumColor.DARK, 7, 6, knightDark2Img));
         chessBoard.addPiece(new Rook(EnumColor.DARK, 7, 0, rookDark1Img));
         chessBoard.addPiece(new Rook(EnumColor.DARK, 7, 7, rookDark2Img));
-        chessBoard.addPiece(new King(EnumColor.DARK, 4, 7, kingDarkImg));
+        chessBoard.addPiece(new King(EnumColor.DARK, 7, 4, kingDarkImg));
         chessBoard.addPiece(new Queen(EnumColor.DARK, 7, 3, queenDarkImg));
-
-        chessBoard.getPiece(4, 7).getImage().setLayoutParams(chessBoard.getSquared(4, 7).getLayoutParams());
-        chessBoard.getPiece(4, 7).getImage().setLeft(chessBoard.getSquared(4, 7).getLeft());
-        chessBoard.getPiece(4, 7).getImage().setTop(chessBoard.getSquared(4, 7).getTop());
-        chessBoard.getPiece(4, 7).getImage().setTop(chessBoard.getSquared(4, 7).getTop());
-//        chessBoard.getPiece(7, 4).getImage().getLayoutParams().width =
-        chessBoardGrid.addView(chessBoard.getPiece(4, 7).getImage());
-        //UM CAMINHO PARA ADICIONAR AS PEÇAS
+        chessBoard.addPiece(new Bishop(EnumColor.WHITE, 0, 2, bishopWhite1Img));
+        chessBoard.addPiece(new Bishop(EnumColor.WHITE, 0, 5, bishopWhite2Img));
+        chessBoard.addPiece(new Knight(EnumColor.WHITE, 0, 1, knightWhite1Img));
+        chessBoard.addPiece(new Knight(EnumColor.WHITE, 0, 6, knightWhite2Img));
+        chessBoard.addPiece(new Rook(EnumColor.WHITE, 0, 0, rookWhite1Img));
+        chessBoard.addPiece(new Rook(EnumColor.WHITE, 0, 7, rookWhite2Img));
+        chessBoard.addPiece(new King(EnumColor.WHITE, 0, 4, kingWhiteImg));
+        chessBoard.addPiece(new Queen(EnumColor.WHITE, 0, 3, queenWhiteImg));
+        chessBoard.getPiece(7, 0).getImage().setLayoutParams(chessBoard.getSquared(7, 0).getLayoutParams());
+        chessBoard.getPiece(7, 1).getImage().setLayoutParams(chessBoard.getSquared(7, 1).getLayoutParams());
+        chessBoard.getPiece(7, 2).getImage().setLayoutParams(chessBoard.getSquared(7, 2).getLayoutParams());
+        chessBoard.getPiece(7, 3).getImage().setLayoutParams(chessBoard.getSquared(7, 3).getLayoutParams());
+        chessBoard.getPiece(7, 4).getImage().setLayoutParams(chessBoard.getSquared(7, 4).getLayoutParams());
+        chessBoard.getPiece(7, 5).getImage().setLayoutParams(chessBoard.getSquared(7, 5).getLayoutParams());
+        chessBoard.getPiece(7, 6).getImage().setLayoutParams(chessBoard.getSquared(7, 6).getLayoutParams());
+        chessBoard.getPiece(7, 7).getImage().setLayoutParams(chessBoard.getSquared(7, 7).getLayoutParams());
+        chessBoard.getPiece(0, 0).getImage().setLayoutParams(chessBoard.getSquared(0, 0).getLayoutParams());
+        chessBoard.getPiece(0, 1).getImage().setLayoutParams(chessBoard.getSquared(0, 1).getLayoutParams());
+        chessBoard.getPiece(0, 2).getImage().setLayoutParams(chessBoard.getSquared(0, 2).getLayoutParams());
+        chessBoard.getPiece(0, 3).getImage().setLayoutParams(chessBoard.getSquared(0, 3).getLayoutParams());
+        chessBoard.getPiece(0, 4).getImage().setLayoutParams(chessBoard.getSquared(0, 4).getLayoutParams());
+        chessBoard.getPiece(0, 5).getImage().setLayoutParams(chessBoard.getSquared(0, 5).getLayoutParams());
+        chessBoard.getPiece(0, 6).getImage().setLayoutParams(chessBoard.getSquared(0, 6).getLayoutParams());
+        chessBoard.getPiece(0, 7).getImage().setLayoutParams(chessBoard.getSquared(0, 7).getLayoutParams());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 0).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 1).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 2).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 3).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 4).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 5).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 6).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(7, 7).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 0).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 1).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 2).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 3).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 4).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 5).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 6).getImage());
+        chessBoardGrid.addView(chessBoard.getPiece(0, 7).getImage());
 
 
     }
